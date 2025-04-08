218ea24aae447dc24327ea50ec20b4cecf937cc7
Siddhant Agre <siddhantagre01@gmail.com>
2025-04-08 19:05:24 +0530

    Switch to HashRouter and update vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Multi-page-navigation/',
   plugins: [tailwindcss(), react()],
 });
